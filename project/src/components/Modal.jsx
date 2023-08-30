8225a77f9a23b2aa285c6e6359c2c628fbbc70b0
KESTUTIS-LAPTOP\udamb <u.dambrauske@gmail.com>
2023-08-30 07:45:47 +0300

    exit modal function added

--- project/src/components/Modal.jsx
+++ project/src/components/Modal.jsx
@@ -21,6 +21,7 @@ const Modal = () => {
             <div className={"fixed top-0 border-2 left-0 right-0 w-full h-full backdrop-blur-sm bg-black bg-opacity-50 z-20"}></div>
             <div className={"bg-slate-100 h-40 w-96 flex justify-center items-center flex-col gap-4 absolute top-20 z-30 rounded"}>
                 <div
+                    onClick={() => dispatch(setModal(false))}
                     className={"i-cont absolute top-3 right-3 border-2 border-slate-400 w-8 h-8 flex justify-center items-center rounded hover:bg-slate-500 cursor-pointer hover:border-0"}>
                     <i className="fas fa-times text-xl"></i>
                 </div>
